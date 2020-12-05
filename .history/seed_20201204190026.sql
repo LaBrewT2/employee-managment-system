94c4ae6a58402eee1caa67a30d75e7fec1682c75
LaBrew T2 <labrewt2@hawkmail.newpaltz.edu>
2020-12-05 09:57:48 -0500

    sql

---
+++ .history/seed_20201204190026.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_tracker;
+-- Created the DB "employee_tracker" 
+CREATE DATABASE employee_tracker;
+-- Use the DB employee_tracker for all the rest of the script
+USE employee_tracker;
+-- Created "departments" table
+CREATE TABLE department (
+id INT (10) ,
+name VARCHAR(30) NULL
+);
+-- Created "role" table
+CREATE TABLE role (
+id INT (10) ,
+title VARCHAR(30) NULL,
+salary DECIMAL(10,2),
+department_id INT(255) NULL
+);
+-- Created "employees" table
+CREATE TABLE employee (
+id INT(255) NULL,
+first_name VARCHAR(30) NULL,
+last_name VARCHAR(30) NULL,
+role_id INT (255) NULL,
+manager_id INT (255) 
+);
+-- Inserted a set of records into the table
+INSERT INTO employee (id, title, salary ,department_id)
+VALUE (id, title, salary ,department_id);
+SELECT * FROM employees
